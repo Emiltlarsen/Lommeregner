d4b0444cf29f12b0afd950379967f92738e22f1b
Emiltlarsen <emil.t.larsen@gmail.com>
2020-09-25 09:37:44 +0200

    Importering af JS og HTML, samt projektbeskrivelse

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Lommeregner
- 
+
+Projektbeskrivelse
+- Dette projekt vil være en videreudvikling af et tidligere projekt, en DOM-opgave. Opgaven lød på at prøve sig ad med DOM, hvorpå jeg lavede en lommeregner. Input og Output var igennem html og funktioner blev kaldt og udført igennem JS. Lommeregneren skal kunne udføre basale matematiske operatorer mellem to tal. Den skal logge tidligere udregninger og hvis muligt kunne fremfald dem igen og regne videre. 
---
+++ hej.js
@@ -0,0 +1,56 @@
+
+
+function plus(){
+    var a = parseInt(document.getElementById("talInput1").value);
+    var b = parseInt(document.getElementById("talInput2").value);
+    console.log(a + b)
+    return a + b
+}
+
+function minus(){
+    var a = parseInt(document.getElementById("talInput1").value);
+    var b = parseInt(document.getElementById("talInput2").value);
+    console.log(a - b)
+    return a - b
+}
+
+function gange(){
+    var a = parseInt(document.getElementById("talInput1").value);
+    var b = parseInt(document.getElementById("talInput2").value);
+    console.log(a * b)
+    return a * b
+}
+
+function division(){
+    var a = parseInt(document.getElementById("talInput1").value);
+    var b = parseInt(document.getElementById("talInput2").value);
+    console.log(a / b)
+    return a / b
+}
+
+function iAnden(){
+    var a = parseInt(document.getElementById("talInput1").value);
+    console.log(a * a)
+    return a * a
+}
+
+function iX(){
+    var a = parseInt(document.getElementById("talInput1").value);
+    var b = parseInt(document.getElementById("talInput2").value);
+
+    var resultat = a;
+
+    for (let i = 0; i < (b-1); i++) {
+        resultat = resultat * a;
+    }
+    console.log(resultat)
+    return resultat
+}
+
+var pi = 3.1415926535897932384626433832795;
+
+function kvadratRod(){
+    var a = parseInt(document.getElementById("talInput1").value);
+    console.log(Math.sqrt(a));
+    return Math.sqrt(a);
+}
