86d008104e59fc825ca6c8313756f58d212b3bb2
Emiltlarsen <emil.t.larsen@gmail.com>
2020-09-28 17:49:07 +0200

    Rework af to inputfelter til klassisk
    Implementering af knapper, hvorpå man nu skriver et tal ind, vælger operator, evt. skrive endnu et tal og så til sidst regner det.

--- script.js
+++ script.js
@@ -1,54 +1,153 @@
-function plus(){
-    var a = parseInt(document.getElementById("talInput1").value);
-    var b = parseInt(document.getElementById("talInput2").value);
-    console.log(a + b)
-    return a + b
-}
-
-function minus(){
-    var a = parseInt(document.getElementById("talInput1").value);
-    var b = parseInt(document.getElementById("talInput2").value);
-    console.log(a - b)
-    return a - b
-}
-
-function gange(){
-    var a = parseInt(document.getElementById("talInput1").value);
-    var b = parseInt(document.getElementById("talInput2").value);
-    console.log(a * b)
-    return a * b
-}
-
-function division(){
-    var a = parseInt(document.getElementById("talInput1").value);
-    var b = parseInt(document.getElementById("talInput2").value);
-    console.log(a / b)
-    return a / b
-}
-
-function iAnden(){
-    var a = parseInt(document.getElementById("talInput1").value);
-    console.log(a * a)
-    return a * a
-}
-
-function iX(){
-    var a = parseInt(document.getElementById("talInput1").value);
-    var b = parseInt(document.getElementById("talInput2").value);
-
-    var resultat = a;
-
-    for (let i = 0; i < (b-1); i++) {
-        resultat = resultat * a;
-    }
-    console.log(resultat)
-    return resultat
-}
-
+var talIBjaelken = "0";
+var valgteOperator = "";
+var laastTal = "";
 var pi = 3.1415926535897932384626433832795;
 
+
+function tast1(){
+    console.log("Taster 1")
+    if(talIBjaelken === pi || talIBjaelken === "0"){
+        talIBjaelken = "1"
+    } else
+        talIBjaelken = talIBjaelken + "1"
+}
+function tast2(){
+    console.log("Taster 2")
+    if(talIBjaelken === pi || talIBjaelken === "0"){
+        talIBjaelken = "2"
+    } else
+        talIBjaelken = talIBjaelken + "2"
+}
+function tast3(){
+    console.log("Taster 3")
+    if(talIBjaelken === pi || talIBjaelken === "0"){
+        talIBjaelken = "3"
+    } else
+        talIBjaelken = talIBjaelken + "3"
+}
+function tast4(){
+    console.log("Taster 4")
+    if(talIBjaelken === pi || talIBjaelken === "0"){
+        talIBjaelken = "4"
+    } else
+        talIBjaelken = talIBjaelken + "4"
+}
+function tast5(){
+    console.log("Taster 5")
+    if(talIBjaelken === pi || talIBjaelken === "0"){
+        talIBjaelken = "5"
+    } else
+        talIBjaelken = talIBjaelken + "5"
+}
+function tast6(){
+    console.log("Taster 6")
+    if(talIBjaelken === pi || talIBjaelken === "0"){
+        talIBjaelken = "6"
+    } else
+        talIBjaelken = talIBjaelken + "6"
+}
+function tast7(){
+    console.log("Taster 7")
+    if(talIBjaelken === pi || talIBjaelken === "0"){
+        talIBjaelken = "7"
+    } else
+        talIBjaelken = talIBjaelken + "7"
+}
+function tast8(){
+    console.log("Taster 8")
+    if(talIBjaelken === pi || talIBjaelken === "0"){
+        talIBjaelken = "8"
+    } else
+        talIBjaelken = talIBjaelken + "8"
+}
+function tast9(){
+    console.log("Taster 9")
+    if(talIBjaelken === pi || talIBjaelken === "0"){
+        talIBjaelken = "9"
+    } else
+        talIBjaelken = talIBjaelken + "9"
+}
+function tast0(){
+    console.log("Taster 0")
+    if(talIBjaelken === pi || talIBjaelken === "0"){
+        talIBjaelken = "0"
+    } else
+        talIBjaelken = talIBjaelken + "0"
+}
+function tastpi(){
+    talIBjaelken = pi
+    console.log("Taster Pi")
+}
+
+function C(){
+    talIBjaelken = "0"
+    console.log("Sletter...")
+}
+
+function plus(){
+    valgteOperator = "+";
+    laastTal = talIBjaelken;
+    talIBjaelken = "";
+    console.log("Plusser...")
+}
+function minus(){
+    valgteOperator = "-";
+    laastTal = talIBjaelken;
+    talIBjaelken = "";
+    console.log("Minusser...")
+}
+function gange(){
+    valgteOperator = "*";
+    laastTal = talIBjaelken;
+    talIBjaelken = "";
+    console.log("Ganger...")
+}
+function division(){
+    valgteOperator = "/";
+    laastTal = talIBjaelken;
+    talIBjaelken = "";
+    console.log("Dividere...")
+}
+function iAnden(){
+    talIBjaelken = parseInt(talIBjaelken) * parseInt(talIBjaelken);
+    console.log("Opløfter i anden...")
+}
+function iY(){
+    valgteOperator = "x^y";
+    laastTal = talIBjaelken;
+    talIBjaelken = "";
+    console.log("x opløftet i y...")
+}
 function kvadratRod(){
-    var a = parseInt(document.getElementById("talInput1").value);
-    console.log(Math.sqrt(a));
-    return Math.sqrt(a);
+    valgteOperator = "sqrt";
+    talIBjaelken = Math.sqrt(parseInt(talIBjaelken))
+    console.log("Tager kvadratroden...")
+}
+
+
+
+function ligMed(){
+    console.log("Resultat er...")
+    if (valgteOperator === "+") {
+        talIBjaelken = parseInt(talIBjaelken) + parseInt(laastTal)
+    } 
+    else if (valgteOperator === "-") {
+        talIBjaelken = parseInt(talIBjaelken) - parseInt(laastTal)
+    } 
+    else if (valgteOperator === "*") {
+        talIBjaelken = parseInt(talIBjaelken) * parseInt(laastTal)
+    } 
+    else if (valgteOperator === "/") {
+        talIBjaelken = parseInt(talIBjaelken) / parseInt(laastTal)
+    }
+    else if (valgteOperator === "x^y"){
+        var resultat = parseInt(laastTal);
+
+        for (let i = 0; i < (parseInt(talIBjaelken)-1); i++) {
+            resultat = resultat * parseInt(laastTal);
+        }
+    
+        talIBjaelken = resultat;
+    }
+// implementer noget til at slette når du skriver nye tal ind efter du har regnet noget
 }
