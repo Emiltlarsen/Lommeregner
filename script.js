58217428defdd3198113db57b627417216bba06c
Emiltlarsen <emil.t.larsen@gmail.com>
2020-09-30 13:16:19 +0200

    Implementeret DOM

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ var talIBjaelken = "0";
 var valgteOperator = "";
 var laastTal = "";
 var pi = 3.1415926535897932384626433832795;
-var ligeRegnet = 0
+var ligeRegnet = 0;
+
+//document.getElementById("resultater").innerHTML = talIBjaelken
 
 
 function tast1(){
-    console.log("Taster 1")
+    console.log("Taster 1");
     if (ligeRegnet === 1){
         talIBjaelken = "0";
     }
@@ -17,9 +19,11 @@ function tast1(){
     } else
         talIBjaelken = talIBjaelken + "1";
         ligeRegnet = 0;
+
+    document.getElementById("resultater").innerHTML = talIBjaelken;
 }
 function tast2(){
-    console.log("Taster 2")
+    console.log("Taster 2");
     if (ligeRegnet === 1){
         talIBjaelken = "0";
     }
@@ -34,9 +38,11 @@ function tast2(){
     } else
         talIBjaelken = talIBjaelken + "2"
         ligeRegnet = 0;
+
+    document.getElementById("resultater").innerHTML = talIBjaelken;
 }
 function tast3(){
-    console.log("Taster 3")
+    console.log("Taster 3");
     if (ligeRegnet === 1){
         talIBjaelken = "0";
     }
@@ -47,9 +53,11 @@ function tast3(){
     } else
         talIBjaelken = talIBjaelken + "3";
         ligeRegnet = 0;
+
+    document.getElementById("resultater").innerHTML = talIBjaelken;
 }
 function tast4(){
-    console.log("Taster 4")
+    console.log("Taster 4");
     if (ligeRegnet === 1){
         talIBjaelken = "0";
     }
@@ -60,9 +68,12 @@ function tast4(){
     } else
         talIBjaelken = talIBjaelken + "4";
         ligeRegnet = 0;
+
+    document.getElementById("resultater").innerHTML = talIBjaelken;
+
 }
 function tast5(){
-    console.log("Taster 5")
+    console.log("Taster 5");
     if (ligeRegnet === 1){
         talIBjaelken = "0";
     }
@@ -73,9 +84,12 @@ function tast5(){
     } else
         talIBjaelken = talIBjaelken + "5";
         ligeRegnet = 0;
+
+    document.getElementById("resultater").innerHTML = talIBjaelken;
+
 }
 function tast6(){
-    console.log("Taster 6")
+    console.log("Taster 6");
     if (ligeRegnet === 1){
         talIBjaelken = "0";
     }
@@ -86,9 +100,11 @@ function tast6(){
     } else
         talIBjaelken = talIBjaelken + "6";
         ligeRegnet = 0;
+
+    document.getElementById("resultater").innerHTML = talIBjaelken;
 }
 function tast7(){
-    console.log("Taster 7")
+    console.log("Taster 7");
     if (ligeRegnet === 1){
         talIBjaelken = "0";
     }
@@ -99,9 +115,11 @@ function tast7(){
     } else
         talIBjaelken = talIBjaelken + "7";
         ligeRegnet = 0;
+
+    document.getElementById("resultater").innerHTML = talIBjaelken;
 }
 function tast8(){
-    console.log("Taster 8")
+    console.log("Taster 8");
     if (ligeRegnet === 1){
         talIBjaelken = "0";
     }
@@ -112,9 +130,11 @@ function tast8(){
     } else
         talIBjaelken = talIBjaelken + "8";
         ligeRegnet = 0;
+
+    document.getElementById("resultater").innerHTML = talIBjaelken;
 }
 function tast9(){
-    console.log("Taster 9")
+    console.log("Taster 9");
     if (ligeRegnet === 1){
         talIBjaelken = "0";
     }
@@ -125,9 +145,11 @@ function tast9(){
     } else
         talIBjaelken = talIBjaelken + "9";
         ligeRegnet = 0;
+
+    document.getElementById("resultater").innerHTML = talIBjaelken;
 }
 function tast0(){
-    console.log("Taster 0")
+    console.log("Taster 0");
     if (ligeRegnet === 1){
         talIBjaelken = "0";
     }
@@ -138,72 +160,80 @@ function tast0(){
     } else
         talIBjaelken = talIBjaelken + "0";
         ligeRegnet = 0;
+
+    document.getElementById("resultater").innerHTML = talIBjaelken;
 }
 function tastpi(){
     talIBjaelken = pi;
     console.log("Taster Pi");
     ligeRegnet = 0;
+    document.getElementById("resultater").innerHTML = talIBjaelken;
 }
 
 function C(){
-    talIBjaelken = "0"
-    console.log("Sletter...")
+    talIBjaelken = "0";
+    console.log("Sletter...");
     ligeRegnet = 0;
+    document.getElementById("resultater").innerHTML = talIBjaelken;
 }
 
 function plus(){
     valgteOperator = "+";
     laastTal = talIBjaelken;
     talIBjaelken = "";
-    console.log("Plusser...")
+    console.log("Plusser...");
 }
 function minus(){
     valgteOperator = "-";
     laastTal = talIBjaelken;
     talIBjaelken = "";
-    console.log("Minusser...")
+    console.log("Minusser...");
 }
 function gange(){
     valgteOperator = "*";
     laastTal = talIBjaelken;
     talIBjaelken = "";
-    console.log("Ganger...")
+    console.log("Ganger...");
 }
 function division(){
     valgteOperator = "/";
     laastTal = talIBjaelken;
     talIBjaelken = "";
-    console.log("Dividere...")
+    console.log("Dividere...");
 }
 function iAnden(){
     talIBjaelken = parseInt(talIBjaelken) * parseInt(talIBjaelken);
-    console.log("Opløfter i anden...")
+    console.log("Opløfter i anden...");
+    ligeRegnet = 1;
+    document.getElementById("resultater").innerHTML = talIBjaelken;
 }
 function iY(){
     valgteOperator = "x^y";
     laastTal = talIBjaelken;
     talIBjaelken = "";
-    console.log("x opløftet i y...")
+    console.log("x opløftet i y...");
 }
 function kvadratRod(){
     valgteOperator = "sqrt";
     talIBjaelken = Math.sqrt(parseInt(talIBjaelken))
-    console.log("Tager kvadratroden...")
+    console.log("Tager kvadratroden...");
+    ligeRegnet = 1;
+    document.getElementById("resultater").innerHTML = talIBjaelken;
 }
 function plusMinus(){
     if (talIBjaelken === 0) {
-        console.log("Ændrer ikke fortegn på grun af 0")
+        console.log("Ændrer ikke fortegn på grun af 0");
     }
     else if (talIBjaelken != 0) {
         talIBjaelken *= -1
-        console.log("Ændrer fortegn")
+        console.log("Ændrer fortegn");
     }
 }
 
 
 
 function ligMed(){
-    console.log("Resultat er...")
+    console.log("Resultat er...");
     if (valgteOperator === "+") {
         talIBjaelken = parseInt(talIBjaelken) + parseInt(laastTal)
         console.log(talIBjaelken)
@@ -231,6 +261,6 @@ function ligMed(){
         
         console.log(talIBjaelken)
     }
-
+    document.getElementById("resultater").innerHTML = talIBjaelken;
     ligeRegnet = 1
 }
